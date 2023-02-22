2739eeb87438210eb8f5a4a487a8153d9b7a15fd
Yume <hei.hajatiana@gmail.com>
2023-02-22 10:31:00 +0300

    chore(config): webpack to compile cypress react

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
+const webpackPreprocessor = require("@cypress/webpack-preprocessor");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      const options = {
+        webpackOptions: require("./webpack.config"),
+        watchOptions: {},
+      };
+
+      on("file:preprocessor", webpackPreprocessor(options));
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
